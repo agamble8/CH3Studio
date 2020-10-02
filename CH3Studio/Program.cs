fe15597e0140b1d885ad72034ab82256623c0255
Amy Gamble <amymgambl@gmail.com>
2020-10-01 20:54:59 -0500

    finished studio with bonuses

---
+++ CH3Studio/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CH3Studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            string sampleText = Console.ReadLine();
+            //string sampleText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+
+            char[] charactersInString = sampleText.ToCharArray();
+
+            Scorecard.Scorer(charactersInString);
+
+        }
+    }
+}
---
+++ CH3Studio/Scorecard.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Sockets;
+using System.Reflection.PortableExecutable;
+using System.Text;
+
+namespace CH3Studio
+{
+    class Scorecard
+    {
+        
+
+        public static void Scorer (char[] arr)
+        {
+            Dictionary<char, int> characterCount = new Dictionary<char, int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                char letter = Char.ToLower(arr[i]);
+                if (Char.IsLetter(letter))
+                {
+                    if (characterCount.ContainsKey(letter))
+                    {
+                        characterCount[letter]++;
+                    }
+                    else
+                    {
+                        characterCount.Add(letter, 1);
+                    }
+                }
+               
+            }
+
+            foreach (KeyValuePair<char, int> score in characterCount)
+            {
+                Console.WriteLine(score.Key + ": " + score.Value);
+            }
+        }
+    }
+}
